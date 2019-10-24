92c27ed13ed98271bb61071d851b0e1c7e276c13
Thongyos Songsat <thongyos.songsat@hotmail.com>
2019-10-24 16:31:49 +0700

    create bot user

--- disruptapi.js
+++ disruptapi.js
@@ -19,6 +19,25 @@ function GetToken() {
     });
 }
 
+module.exports.crateUser = (whiteLabelName, userType, username, password) => {
+    return new Promise((resolve, reject) => {
+        GetToken().then(access_token => {
+            superagent.post(endpoint + 'api/disrupt/useronline/createBot')
+                        .send({
+                            ListenerName: whiteLabelName,
+                            UserType: userType,
+                            Username: username,
+                            Password: password
+                        }) // sends a JSON post body
+                .set('accept', 'json')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${access_token}`)
+                .then(res => resolve(res.body))
+                .catch(err => reject(err));
+        }).catch(err => reject(err));
+    });
+};
+
 module.exports.clearUserOnline = (whiteLabelName) => {
     return new Promise((resolve, reject) => {
         GetToken().then(access_token => {
